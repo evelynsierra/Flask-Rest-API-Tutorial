af19f74f01864180caf8e0047c036c930ffdd270
Evelyn <sierravelin@gmail.com>
2024-07-14 23:01:53 +0700

    starting JWT tutorial

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request
 
-
 app = Flask(__name__) # creating flask app
 
 stores = [
@@ -16,7 +15,7 @@ stores = [
 ]
 
 @app.get("/store") # localhost:5000/store
-def get_store(): # this is an API endpoint or Flask route
+def get_stores(): # this is an API endpoint or Flask route
     return {"stores":stores}
 
 @app.post("/store")
@@ -36,3 +35,20 @@ def create_item(name):
             store["items"].append(new_item)
             return new_item, 201
     return {"message" : "Store Not Found"}, 404
+
+@app.get('/store/<string:name>')
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return {"message" : "Store Not Found"}, 404
+
+@app.get('/store/<string:name>/item')
+def get_items(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}
+    return {"message" : "Store Not Found"}, 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
